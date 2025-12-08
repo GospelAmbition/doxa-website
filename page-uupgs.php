77c3dc959856eecbe0e28ed4166bf8cab5a0745b
Nath <nathinabob@gmail.com>
2025-12-08 15:04:17 +0000

    feat: add button styles

--- page-uupgs.php
+++ page-uupgs.php
@@ -15,6 +15,12 @@ get_header( 'top' ); ?>
         <div class="container page-content uupgs-page">
             <!-- Custom content for UUPGS page goes here -->
             <h1 class="text-center"><?php echo __('Find a UUPG to adopt', 'doxa-website'); ?></h1>
+            <a class="button" href="<?php echo home_url('/uupgs'); ?>"><?php echo __('Reset filters', 'doxa-website'); ?></a>
+            <a class="button back-button" href="<?php echo home_url('/pray'); ?>"><?php echo __('Back to prayer', 'doxa-website'); ?></a>
+            <div class="filter-buttons">
+                <button class="button filter-button"><?php echo __('Blah', 'doxa-website'); ?></button>
+                <button class="button filter-button"><?php echo __('Foo', 'doxa-website'); ?></button>
+            </div>
         </div>
     </main>
 
