3c4e1aa57d4bbc59a9e93464eed4b9d18260920e
Nath <nathinabob@gmail.com>
2025-12-18 16:45:48 +0000

    refactor: surface style names

--- front-page.php
+++ front-page.php
@@ -10,8 +10,8 @@ get_header( 'top' ); ?>
     <?php get_header(); ?>
 
     <main class="site-main">
-        <div class="container page-content front-page">
-            <section class="stack stack--md">
+        <div class="page-content front-page">
+            <section class="stack stack--md container">
                 <div>
                     <h2 class="color-brand"><?php echo __('Our gift to Jesus', 'doxa-website'); ?>:</h2>
                     <h1 class="color-brand-light highlight-first highlight-last" data-highlight-color="primary"><?php echo __('Engage every people by 2033', 'doxa-website'); ?></h1>
@@ -19,6 +19,14 @@ get_header( 'top' ); ?>
                 <div>
                     <!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/images/front-page-image.jpg" alt="<?php echo __('Engage every people by 2033', 'doxa-website'); ?>"> -->
                     <img src="https://placehold.co/1200x600" alt="<?php echo __('Engage every people by 2033', 'doxa-website'); ?>">
+                </div>
+            </section>
+            <section class="stack stack--md | surface-brand">
+                <div class="container stack stack--xl">
+                    <h2 class="highlight-first"><?php echo sprintf( __( '%s unengaged people groups', 'doxa-website'), '2,085' ); ?></h2>
+                    <p class="subtext">
+                        <?php echo __('Our hope is to see each one of them covered in 24-hour prayer, and your church can be part of it.', 'doxa-website'); ?>
+                    </p>
                 </div>
             </section>
         </div>
