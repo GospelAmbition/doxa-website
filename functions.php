00332d099c31bdd2ed1685d983e9b7fe790386d8
Nath <nathinabob@gmail.com>
2026-01-07 17:04:34 +0000

    fix: put back in live api server address

--- functions.php
+++ functions.php
@@ -447,7 +447,7 @@ function custom_adoption_form_template($template) {
 add_filter('template_include', 'custom_adoption_form_template');
 
 function get_uupg_by_slug( $slug ) {
-    $api_url = 'https://uupg.doxa.live/wp-json/dt-public/disciple-tools-people-groups-api/v1/detail/' . urlencode($slug);
+    $api_url = 'https://uupg.doxa.life/wp-json/dt-public/disciple-tools-people-groups-api/v1/detail/' . urlencode($slug);
 
     $response = wp_remote_get($api_url);
     if (is_wp_error($response)) {
