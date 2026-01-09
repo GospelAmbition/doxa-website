823f9379b21300f4421c688f15d33f457b1723ab
Nath <nathinabob@gmail.com>
2026-01-09 13:56:42 +0000

    feat: add contact us form fields

--- page-contact.php
+++ page-contact.php
@@ -5,32 +5,38 @@
  * A contact page template
  */
 
-get_header(); ?>
+get_header( 'top' ); ?>
 
-<main class="site-main max-width-template">
-    <?php if (have_posts()) : ?>
-        <?php while (have_posts()) : the_post(); ?>
-            <article id="page-<?php the_ID(); ?>" <?php post_class('page-content max-width-page'); ?>>
-                <?php if (has_post_thumbnail()) : ?>
-                    <div class="page-featured-image max-width">
-                        <?php the_post_thumbnail('large'); ?>
-                    </div>
-                <?php endif; ?>
+<div class="page">
 
-                <div class="page-body max-width">
-                    <?php the_content(); ?>
+    <?php get_header(); ?>
 
-                    <?php
-                    wp_link_pages(array(
-                        'before' => '<div class="page-links">',
-                        'after'  => '</div>',
-                    ));
-                    ?>
+    <main class="site-main">
+        <div class="container page-content">
+
+            <h1 class="page-title"><?php echo esc_html__('Contact Us', 'doxa-website'); ?></h1>
+
+            <form action="" class="stack stack--md">
+                <div class="">
+                    <label for="name"><?php echo esc_html__('Name', 'doxa-website'); ?></label>
+                    <input type="text" id="name" name="name" required placeholder="<?php echo esc_attr__('Enter your name', 'doxa-website'); ?>">
                 </div>
-            </article>
+                <div class="">
+                    <label for="email"><?php echo esc_html__('Email', 'doxa-website'); ?></label>
+                    <input type="email" id="email" name="email" required placeholder="<?php echo esc_attr__('Enter your email', 'doxa-website'); ?>">
+                </div>
+                <div class="">
+                    <label for="message"><?php echo esc_html__('Message', 'doxa-website'); ?></label>
+                    <textarea id="message" name="message" rows="5" required placeholder="<?php echo esc_attr__('Enter your message', 'doxa-website'); ?>"></textarea>
+                </div>
+                <button type="submit" class="button"><?php echo esc_html__('Submit', 'doxa-website'); ?></button>
+            </form>
 
-        <?php endwhile; ?>
-    <?php endif; ?>
-</main>
+        </div>
+    </main>
 
-<?php get_footer(); ?>
+    <?php get_footer(); ?>
+
+</div>
+
+<?php get_footer( 'bottom' ); ?>
