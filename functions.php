9a35847d98b763766e6ab04e56e220cb8d3707f6
Nath <nathinabob@gmail.com>
2026-01-09 14:27:06 +0000

    fix: stop login and admin page redirecting to coming soon

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if (!defined('ABSPATH')) {
 }
 
 function coming_soon_redirect() {
-    if ( !is_page( 'coming-soon' ) && !is_user_logged_in() ) {
+    if ( !is_admin() && !is_page( 'coming-soon' ) && !is_user_logged_in() ) {
         wp_redirect( home_url( '/coming-soon' ) );
         exit;
     }
